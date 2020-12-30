d0d47f148a9e2c493d87658c7d23a36f1efb07d3
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:29 +0530

    lynx: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-calyx.mk
+++ device-calyx.mk
@@ -4,3 +4,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/lynx/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/lynx/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/lynx/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
